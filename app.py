cfe1688a9761844ec806bb33c431a1c7c625c4e7
BabyPanda <babypandaboyz@gmail.com>
2025-07-20 22:17:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -284,5 +284,5 @@ def check_url():
         return jsonify({"error": "Server processing error"}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
